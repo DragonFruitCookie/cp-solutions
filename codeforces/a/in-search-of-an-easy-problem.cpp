9acac7144ed03e1d02dff9021ada3997af3c5854
Revathi V <103417496+DragonFruitCookie@users.noreply.github.com>
2025-05-12 19:29:51 +0530

    Create in-search-of-an-easy-problem.cpp

---
+++ codeforces/a/in-search-of-an-easy-problem.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	int a = 0;
+	while(t--){
+	    int c;
+	    cin>>c;
+	    if(c==1) a=1;
+	}
+	if(a==1) cout<<"hard";
+	else cout<<"easy";
+	
+	return 0;
+}
