56c1a534e5a41b37f85948f4196088180ae7dbc9
Revathi V <103417496+DragonFruitCookie@users.noreply.github.com>
2025-05-23 12:29:09 +0530

    Create Required Remainder.cpp

---
+++ codeforces/a/Required Remainder.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t; cin>>t;
+	while(t--){
+	    int x, y, n;
+	    cin>>x>>y>>n;
+	    int r=n%x;
+	    if(r==y) cout<<n<<endl;
+	    else if(r>y) {
+	        cout<<n-(r-y)<<endl;
+	    }
+	    else{
+	        cout<<((n/x)-1)*x+y<<endl;
+	    }
+	}
+	return 0;
+}
