e58c776ef7772e7605b215d8ed3c286708482605
Revathi V <103417496+DragonFruitCookie@users.noreply.github.com>
2025-05-23 09:32:09 +0530

    Create LCM Problem.cpp

---
+++ codeforces/a/LCM Problem.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t; cin>>t;
+	while(t--){
+	    int a, b;
+	    cin>>a>>b;
+	    if(a*2>b) cout<<-1<<" "<<-1<<endl;
+	    else cout<<a<<" "<<a*2<<endl; 
+	}
+	return 0;
+}
