ed04d66091e90098dcc6d16b47f041744b6e45d3
Revathi V <103417496+DragonFruitCookie@users.noreply.github.com>
2025-05-23 12:50:26 +0530

    Create Magical Sticks.cpp

---
+++ codeforces/a/Magical Sticks.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t; cin>>t;
+	while(t--){
+	    int n; cin>>n;
+	    int st=1;
+	    st+=(n-1)/2;
+	    cout<<st<<endl;
+	}
+	return 0;
+}
